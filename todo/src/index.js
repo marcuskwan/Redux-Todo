46f92f5613f392cc9375d9a36efa50b75d6ca3a7
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-19 05:48:17 +1000

    imported provider component, reducer fn, createstore method into index js src

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+//importing createStore
+import { createStore } from "redux";
+// importing Provider
+import { Provider } from "react-redux";
+// importing reducer
+import { reducer } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create store, linking it with reducer fn
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
